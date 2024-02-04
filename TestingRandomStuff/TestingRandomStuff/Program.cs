18a1ff7ca1145052f756d61e075781ca56ae8507
Per Nielsen <pernielsen8700@gmail.com>
2024-02-04 14:00:44 +0100

    Added application to test random stuff

---
+++ TestingRandomStuff/TestingRandomStuff/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hvad hedder du?");
+string navn = Console.ReadLine();
+
+Console.WriteLine("Hvor gammel er du?");
+int alder = Convert.ToInt32(Console.ReadLine());
+
+string nyAlder = (alder + 1).ToString();
+
+Console.WriteLine($"Hej {navn}");
+Console.WriteLine($"Til næste år bliver du {nyAlder} år");
