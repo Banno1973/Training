30cbf7d24ed95535e027ff601b1785e3aaf8b40b
Per Nielsen <pernielsen8700@gmail.com>
2024-04-20 13:25:10 +0200

    Commit all

---
+++ Eksamensprojekt/Gladiator/Gladiator/Domain/Models/Character.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gladiator.Domain.Models
+{
+    internal class Character
+    {
+        private int attackPower;
+        private int defenseRating;
+        private int lifePoints;
+
+        protected int AttackPower { get => attackPower; set => attackPower = value; }
+        protected int DefenseRating { get => defenseRating; set => defenseRating = value; }
+        protected int LifePoints { get => lifePoints; set => lifePoints = value; }
+    }
+}
